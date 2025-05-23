13dc5afff5f213ac5d1ef260abf00d5fe663e581
gilyazov <gilyazov.aydar@gmail.com>
2025-05-23 18:57:21 +0300

    фикс

--- src/js/fancybox.ts
+++ src/js/fancybox.ts
@@ -5,4 +5,28 @@ export default function fancybox() {
   Fancybox.bind("[data-fancybox]", {
     placeFocusBack: false,
   });
+
+  const cards = document.querySelectorAll('.reviews__video-card');
+  cards.forEach(function (card){
+    card.addEventListener('click', function(e) {
+      e.preventDefault();
+      // @ts-ignore
+      const id = card.dataset.id;
+      // @ts-ignore
+      const request = BX.ajax.runAction('gilyazov:core.api.reviews.iframe', {
+        data: {
+          id: id
+        }
+      });
+      // @ts-ignore
+      request.then(function(response){
+        Fancybox.show([{
+          // @ts-ignore
+          placeFocusBack: false,
+          src: '<div class="reviews-russian-video reviews-russian-video--vk">'+response.data.iframe+'</div>',
+          type: 'html'
+        }]);
+      });
+    });
+  });
 }
