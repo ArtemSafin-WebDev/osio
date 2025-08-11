99c3fda021727a2200561b9b7c7ffb227e871c44
gilyazov <gilyazov.aydar@gmail.com>
2025-08-11 12:22:25 +0300

    финальный фикс

--- src/js/fancybox.ts
+++ src/js/fancybox.ts
@@ -9,9 +9,13 @@ export default function fancybox() {
   const BTN_SELECTOR = '.reviews__video-card';
   document.addEventListener("click", (event) => {
     const target = event.target;
-    console.log(event.target)
-    // @ts-ignore
-    if (target.matches(BTN_SELECTOR)) {
+
+    if (
+        // @ts-ignore
+        target.matches(BTN_SELECTOR) ||
+        // @ts-ignore
+        target.closest(BTN_SELECTOR)
+    ) {
       event.preventDefault();
 
       // @ts-ignore
